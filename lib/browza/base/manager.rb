ff25a453e1d010421735f0f4ed6ef3ff5dcc8e4a
Kim, Peter <Peter.Kim@concur.com>
2017-04-13 07:44:53 -0400

    Add initial support for multi-browser testing.

--- lib/browza/base/manager.rb
+++ lib/browza/base/manager.rb
@@ -11,12 +11,25 @@ class Manager
 
   attr_accessor :drv
   attr_accessor :appModels
-  attr_accessor  :browserType
+  attr_accessor :browserType
+  attr_accessor :defaultTimeout
+  attr_accessor :browserList
 
   def initialize(logLevel = :warn)
+    @browserList=[]
     @logger = Logging.logger(STDOUT)
     @logger.level = logLevel
+    @defaultTimeout = 30
     @appModels=[]
+  end
+
+  def setLogLevel(l)
+    puts __FILE__ + (__LINE__).to_s + " setLogLevel(#{l})"
+    @logger.level = l
+  end
+
+  def setTimeout(s)
+    @defaultTimeout = s
   end
 
   def addModel(_a)
@@ -38,6 +51,8 @@ class Manager
     @browserType = _type
     @drv = Selenium::WebDriver.for @browserType
     setDimension
+
+    @browserList << @drv
   end
 
   def getDriver()
@@ -59,7 +74,11 @@ class Manager
   end
 
   def goto(url)
-    getDriver().navigate.to url
+
+    @browserList.each do |b|
+      b.navigate.to url
+    end
+    #getDriver().navigate.to url
   end
 
   def navigate(url)
@@ -143,6 +162,7 @@ class Manager
       @logger.warn __FILE__ + (__LINE__).to_s + " NoSuchElementError : #{locator}"
 
     rescue => ex
+      @logger.warn __FILE__ + (__LINE__).to_s + " #{ex.class}"
       @logger.warn "Error during processing: #{$!}"
       @logger.warn "Backtrace:\n\t#{ex.backtrace.join("\n\t")}"
     end
@@ -219,9 +239,9 @@ class Manager
   end
 
 
-  def switch_frame(e)
+  def switch_frame(e, drv=nil)
 
-    drv = @drv
+    drv = @drv if drv.nil?
     @logger.debug __FILE__ + (__LINE__).to_s + "\n\n== self.switch_frame(#{e}) =="
     frames=nil
     if e.is_a?(Hash) && e.has_key?('page') && e['page'].has_key?('frames')
@@ -288,7 +308,9 @@ class Manager
     end
   end
 
-  def findLocator(_locator)
+  def findLocator(_locator, drv=nil)
+    drv = @drv if drv.nil?
+
     @logger.debug __FILE__ + (__LINE__).to_s + " [findLocator]: #{_locator}   sz: #{@appModels.length}"
     obj = nil
     _hit = nil
@@ -337,17 +359,32 @@ class Manager
     end
 
     if _hit.is_a?(Hash)
-      if _hit.has_key?('frame')
-        @logger.debug __FILE__ + (__LINE__).to_s + "swtich_to_frame : #{_hit['frame']}"
-        switch_frame(_hit['frame'])
-      end
 
-      if _hit.has_key?('locator')
-        obj = getElement(_hit['locator'], @drv, 30)
-      end
+      2.times {
+
+        begin
+          if _hit.has_key?('frame')
+            @logger.debug __FILE__ + (__LINE__).to_s + "swtich_to_frame : #{_hit['frame']}"
+            switch_frame(_hit['frame'], drv)
+          end
+
+          if _hit.has_key?('locator')
+            obj = getElement(_hit['locator'], drv, @defaultTimeout)
+          end
+
+          if !obj.nil?
+            break
+          end
+
+          sleep(0.25)
+
+        rescue => e
+          ;
+        end
+      }
 
     else
-      obj = getElement(Appmodel::Model.toBy(_locator), @drv, 30)
+      obj = getElement(Appmodel::Model.toBy(_locator), drv, @defaultTimeout)
     end
 
     @logger.debug __FILE__ + (__LINE__).to_s + " [return findLocator(#{_locator})] : #{_hit}"
@@ -363,58 +400,75 @@ class Manager
   def click(_locator, _drv=nil, _timeout=30)
     rc = false
 
-#    obj = getElement(findLocator(_locator), _drv, _timeout)
-    @drv.switch_to.default_content
-    obj = findLocator(_locator)
-    if !obj.nil?
-      obj.click
-      rc=true
+    @browserList.each do |drv|
+      begin
+        drv.switch_to.default_content
+        obj = findLocator(_locator, drv)
+        if !obj.nil?
+          obj.click
+          rc=true
+        end
+      rescue => ex
+        @logger.warn __FILE__ + (__LINE__).to_s + " #{ex.class}"
+      end
     end
 
     rc
   end
 
-  def highlight(_locator, _drv=nil, _timeout=30)
+  def highlight(_locator, color='red', _drv=nil, _timeout=30)
     rc = false
-    style={"color" => 'rgb(255, 16, 16)'}
-    color="rgb(255, 0, 0)"
+    rgb=nil
 
     obj = findLocator(_locator)
     if !obj.nil?
-
-
-      if style.has_key?("color")
-
-        color=style.has_key?("color")? style["color"] : 'rgb(255, 16, 16)'
-
-        _c = style["color"]
-
-        # TODO: refactor with command 'highlight.rb'
-
-        if _c.match(/\s*blue/i)
-          color='rgb(0, 0, 255)'
-        elsif _c.match(/\s*red/i)
-          color='rgb(255, 0, 0)'
-        elsif _c.match(/\s*yellow/i)
-          color='rgb(255, 255, 0)'
-        elsif _c.match(/\s*green/i)
-          color='rgb(0, 255, 0)'
-        elsif _c.match(/\s*gray/i)
-          color='rgb(128, 128, 128)'
-        end
-
+      if color.match(/\s*blue/i)
+        rgb='rgb(0, 0, 255)'
+      elsif color.match(/\s*red/i)
+        rgb='rgb(255, 0, 0)'
+      elsif color.match(/\s*yellow/i)
+        rgb='rgb(255, 255, 0)'
+      elsif color.match(/\s*green/i)
+        rgb='rgb(0, 255, 0)'
+      elsif color.match(/\s*gray/i)
+        rgb='rgb(128, 128, 128)'
       end
 
-      border=style.has_key?("border")? style["border"] : 1
+      border = 2
 
-      parents = ""
+      begin
+        @drv.execute_script("hlt = function(c) { c.style.border='solid #{border}px #{rgb}'; }; return hlt(arguments[0]);", obj)
+        rc=true
+      rescue => ex
+        @logger.warn __FILE__ + (__LINE__).to_s + " #{ex.class}"
+      end
 
-      @drv.execute_script("hlt = function(c) { c.style.border='solid #{border}px #{color}'; }; return hlt(arguments[0]);", obj)
-      rc=true
     end
 
     obj.is_a?(Selenium::WebDriver::Element) && rc
   end
+
+  def hover(_locator)
+    rc = false
+
+    obj = findLocator(_locator)
+    if !obj.nil?
+      begin
+        @drv.action.move_to(obj).perform
+        rc = true
+      rescue => ex
+        @logger.warn "Error during processing: #{$!} - #{ex.class}"
+        @logger.warn "Backtrace:\n\t#{ex.backtrace.join("\n\t")}"
+      end
+
+    else
+      @logger.debug __FILE__ + (__LINE__).to_s + " hover(#{_locator}) not found."
+    end
+
+    @logger.debug __FILE__ + (__LINE__).to_s + " hover(#{_locator}) : #{rc}"
+    rc
+  end
+
 
   def type(_locator, _text, _timeout=30)
     rc = false
@@ -422,8 +476,30 @@ class Manager
 #    obj = getElement(findLocator(_locator), _drv, _timeout)
     obj = findLocator(_locator)
     if !obj.nil?
-      obj.send_keys(_text)
-      rc=true
+
+      if _text.match(/\s*__CLEAR__\s*$/)
+        _text = :clear
+      elsif _text.match(/\s*__DOWN__\s*$/)
+        _text = :arrow_down
+      elsif _text.match(/\s*__LEFT__\s*$/)
+        _text = :arrow_left
+      elsif _text.match(/\s*__RIGHT__\s*$/)
+        _text = :arrow_right
+      elsif _text.match(/\s*__UP__\s*$/)
+        _text = :arrow_up
+      elsif _text.match(/\s*__TAB__\s*$/)
+        _text = :tab
+      elsif _text.match(/\s*__SHIFT_TAB__\s*$/)
+        @drv.action.key_down(:shift).send_keys(:tab).perform
+        @drv.action.key_up(:shift).perform
+        rc = true
+      end
+
+      unless rc
+        obj.send_keys(_text)
+        rc=true
+      end
+
     end
 
     rc
@@ -450,6 +526,23 @@ class Manager
     activeElt.attribute('value')
   end
 
+  def isFocused?(_locator)
+
+    rc = false
+    activeElt = @drv.switch_to.active_element
+    obj = findLocator(_locator)
+
+
+    if !obj.nil?
+      rc = (activeElt == obj)
+    end
+
+    rc
+
+  end
+
+
+
   def press(k, n=1)
 
     n.times {
@@ -468,13 +561,25 @@ class Manager
       elsif k.match(/\s*^up/i)
         activeElt = @drv.switch_to.active_element
         activeElt.send_keys(:arrow_up)
+      elsif k.match(/\s*__SHIFT_TAB__\s*$/i)
+        @drv.action.key_down(:shift).send_keys(:tab).perform
+        @drv.action.key_up(:shift).perform
       else
         break
       end
     }
   end
 
+  def selected?(_locator, _drv=nil, _timeout=30)
+    rc=false
 
+#    obj=getElement(findLocator(_locator), _drv, _timeout)
+    obj = findLocator(_locator)
+    if !obj.nil?
+      rc=obj.selected?
+    end
+    rc
+  end
 
   def getCount(_locator, _drv=nil, _timeout=30)
     rc=0
